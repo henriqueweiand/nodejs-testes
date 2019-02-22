8cd689e278d9d73676d0090aea6793c94589df79
Henrique Weiand <henriqueweiand@gmail.com>
2019-02-22 17:52:18 -0300

    struct base

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', (req, res) => {
+  res.send('Ola');
+});
+
+app.listen(3000);
